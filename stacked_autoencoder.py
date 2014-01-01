a7a370581cab7d7798327b5e9ac00acbd325762c
dusano <dusan.omercevic@zemanta.com>
2014-01-01 15:12:15 +0100

    Full implementation of stacked_autoencoder

--- stacked_autoencoder.py
+++ stacked_autoencoder.py
@@ -1,20 +1,24 @@
 # Based on CS294A/CS294W Programming Assignment Starter Code
 from numpy  import *
+from scipy.sparse import *
 
+from compute_numerical_gradient import computeNumericalGradient
 import sparse_autoencoder
 
 
 class Layer:
-	W = None
-	b = None
 	
 	def __init__(self, num):
 		self.num = num
+		self.W = None
+		self.b = None
 	
 	
 class NetConfig:
-	inputSize = 0
-	layerSizes = []
+	
+	def __init__(self):
+		self.inputSize = 0
+		self.layerSizes = []
 
 
 def stack2params(stack):
@@ -31,23 +35,21 @@ def stack2params(stack):
 	 
 	""" 
 	params = array([])
-	prev_layer = None
-	for layer in stack:
-		params = concatenate([params, layer.W.ravel(), layer.b.ravel()])
-	
-		assert layer.W.shape[0] == layer.b.shape[0], 'The bias should be a *column* vector of %i x1' % layer.W.shape[0]
-		
-		if prev_layer is not None:
-			assert prev_layer.W.shape[0] == layer.W.shape[1], \
-					'The adjacent layers L%i and L%i should have matching sizes.' % (prev_layer.num, layer.num)
-
-		prev_layer = layer
-	
 	netConfig = NetConfig()
 	if len(stack) > 0:
+		prev_layer = None
 		netConfig.inputSize = stack[0].W.shape[1]
 		for layer in stack:
+			assert layer.W.shape[0] == layer.b.shape[0], 'The bias should be a *column* vector of %i x1' % layer.W.shape[0]
+			
+			if prev_layer is not None:
+				assert prev_layer.W.shape[0] == layer.W.shape[1], \
+						'The adjacent layers L%i and L%i should have matching sizes.' % (prev_layer.num, layer.num)
+						
+			params = concatenate([params, layer.W.ravel(), layer.b.ravel()])
 			netConfig.layerSizes.append(layer.W.shape[0])
+	
+			prev_layer = layer
 	
 	return (params, netConfig)
 
@@ -69,20 +71,75 @@ def params2stack(params, netConfig):
 	for layerSize in netConfig.layerSizes:
 		layerNum += 1
 		layer = Layer(layerNum)
-		
-		wlen = layerSize * prevLayerSize
-		layer.W = params[curPos:curPos+wlen].reshape(layerSize, prevLayerSize)
-		curPos += wlen
-		
-		blen = layerSize
-		layer.b = params[curPos:curPos+blen].ravel()
-		curPos += blen
+
+		layer.W = params[curPos:curPos+layerSize * prevLayerSize].reshape(layerSize, prevLayerSize)
+		curPos += layerSize * prevLayerSize
+
+		layer.b = params[curPos:curPos+layerSize].ravel()
+		curPos += layerSize
 		
 		prevLayerSize = layerSize
 		
 		stack.append(layer)
 	
 	return stack
+
+
+def cost(thetaParam, inputSize, hiddenSize, numClasses, netConfig, lambdaParam, data, labels):
+	"""Takes a trained softmaxTheta and a training data set with labels, and returns cost
+	and gradient using a stacked autoencoder model. Used for finetuning.
+	
+	Keyword arguments:
+	thetaParam -- trained weights from the autoencoder
+	visibleSize -- the number of input units
+	hiddenSize --  the number of hidden units *at the 2nd layer*
+	numClasses --  the number of categories
+	netConfig --   the network configuration of the stack
+	lambdaParam -- the weight regularization penalty
+	data -- our matrix containing the training data as columns.  So, data[i,:] is the i-th training example. 
+	labels -- a vector containing labels, where labels[i] is the label for the i-th training example
+	
+	"""
+	
+	# We first extract the part which compute the softmax gradient
+	softmaxTheta = thetaParam[0:hiddenSize*numClasses].reshape(numClasses, hiddenSize)
+	stack = params2stack(thetaParam[hiddenSize*numClasses:], netConfig)
+	
+	m = data.shape[0]
+	groundTruth = array(csc_matrix( (ones(m),(labels,range(m))), shape=(numClasses,m) ).todense())
+
+	activation = data
+	activations = []
+	for layer in stack:
+		activations.append(activation)
+		activation = sparse_autoencoder.sigmoid(activation.dot(layer.W.T) + layer.b)
+
+	M = softmaxTheta.dot(activation.T)
+	M = M - amax(M, 0)
+	h_data = exp(M)
+	h_data = h_data / sum(h_data, 0)
+	
+	cost = -1.0/numClasses * sum(multiply(groundTruth, log(h_data))) + lambdaParam/2 * sum(softmaxTheta**2)
+	softmaxThetaGrad = -1.0/numClasses * ((groundTruth - h_data).dot(activation)) + lambdaParam*softmaxTheta
+	
+	stackGrad = []
+	delta = multiply(-(softmaxTheta.T.dot(groundTruth - h_data)), (activation * (1-activation)).T)
+	idx = len(activations)
+	while activations != []:
+		activation = activations.pop()
+		layer = Layer(idx)
+		layer.W = (1.0/numClasses) * delta.dot(activation)
+		layer.b = (1.0/numClasses) * sum(delta, 1)
+		stackGrad.insert(0, layer)
+
+		delta = multiply(stack[idx-1].W.T.dot(delta), (activation * (1-activation)).T)
+		
+		idx -= 1
+
+	(params, netConfig) = stack2params(stackGrad)
+	grad = concatenate([softmaxThetaGrad.ravel(), params])
+	
+	return (cost, grad)
 
 
 def predict(thetaParam, inputSize, hiddenSize, numClasses, netConfig, data):
@@ -108,3 +165,36 @@ def predict(thetaParam, inputSize, hiddenSize, numClasses, netConfig, data):
 	h_data = exp(softmaxTheta.dot(activation.T))
 	h_data = h_data / sum(h_data, 0)
 	return argmax(h_data, axis=0)
+
+
+if __name__ == "__main__":
+	inputSize = 4
+	hiddenSize = 5
+	lambdaParam = 0.01
+	data = random.normal(size=(5, inputSize))
+	labels = array([0, 1, 0, 1, 0])
+	numClasses = 2
+	
+	stack = [Layer(1), Layer(2)]
+	stack[0].W = 0.1 * random.normal(size=(3, inputSize))
+	stack[0].b = zeros(3)
+	stack[1].W = 0.1 * random.normal(size=(hiddenSize, 3))
+	stack[1].b = zeros(hiddenSize)
+
+	softmaxTheta = 0.005 * random.normal(size=hiddenSize * numClasses)
+	
+	(stackParams, netConfig) = stack2params(stack)
+	stackedAETheta = concatenate([softmaxTheta, stackParams])
+
+	def stackedAutoencoderCostCallback(x):
+		return cost(x, inputSize, hiddenSize, numClasses, netConfig,
+				lambdaParam, data, labels)
+				
+	(cost_value, grad) = stackedAutoencoderCostCallback(stackedAETheta)
+
+	numgrad = computeNumericalGradient(stackedAutoencoderCostCallback, stackedAETheta)
+	
+	diff = linalg.norm(numgrad-grad)/linalg.norm(numgrad+grad)
+
+	print('%s' % diff)
+	print('Norm of the difference between numerical and analytical gradient (should be < 1e-9)\n\n')
--- stacked_autoencoder_test.py
+++ stacked_autoencoder_test.py
@@ -5,11 +5,13 @@ from numpy  import *
 from scipy import optimize
 
 from MNIST_images import loadMNISTImages, loadMNISTLabels
+from visualize_network import visualizeNetwork
 import sparse_autoencoder
 import softmax
 import stacked_autoencoder
-from visualize_network import visualizeNetwork
 
+
+results_dir = 'results/sae2/'
 
 inputSize = 28 * 28		# MNIST inputs are 28x28
 numClasses = 10			# MNIST dataset consists of 10 digits
@@ -20,6 +22,9 @@ lambdaParam = 3e-3		# weight decay parameter
 betaParam = 3			# weight of sparsity penalty term       
 corruptionLevel = 0.3	# how much of the input to denoising autoencoder will get corrupted.
 
+if not os.path.exists(results_dir):
+	os.makedirs(results_dir)
+
 trainData = loadMNISTImages('mnist/train-images-idx3-ubyte')
 trainLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte')
 
@@ -29,7 +34,7 @@ options = {
 		'disp': True,
 	}
 
-sae1OptThetaFilename = 'sae1OptTheta.npy'
+sae1OptThetaFilename = results_dir + 'sae1OptTheta.npy'
 
 if os.path.exists(sae1OptThetaFilename):
 	sae1OptTheta = load(sae1OptThetaFilename)
@@ -45,12 +50,12 @@ else:
 	save(sae1OptThetaFilename, sae1OptTheta)
 	
 	W1 = sae1OptTheta[0:hiddenSizeL1*inputSize].reshape(hiddenSizeL1, inputSize)
-	visualizeNetwork(W1.T, 'sae1.png')
+	visualizeNetwork(W1.T, results_dir + 'sae1.png')
 
 # Train the second sparse autoencoder
 sae1Features = sparse_autoencoder.feedForward(sae1OptTheta, hiddenSizeL1, inputSize, trainData)
 
-sae2OptThetaFilename = 'sae2OptTheta.npy'
+sae2OptThetaFilename = results_dir + 'sae2OptTheta.npy'
 
 if os.path.exists(sae2OptThetaFilename):
 	sae2OptTheta = load(sae2OptThetaFilename)
@@ -66,7 +71,7 @@ else:
 	save(sae2OptThetaFilename, sae2OptTheta)
 
 # Train the softmax classifier
-saeSoftmaxOptThetaFilename = 'saeSoftmaxOptTheta.npy'
+saeSoftmaxOptThetaFilename = results_dir + 'saeSoftmaxOptTheta.npy'
 
 if os.path.exists(saeSoftmaxOptThetaFilename):
 	saeSoftmaxOptTheta = load(saeSoftmaxOptThetaFilename)
@@ -81,12 +86,12 @@ else:
 	# Randomly initialise theta
 	thetaParam = 0.005 * random.normal(size=numClasses * hiddenSizeL2)
 	
-	options = {
+	softmax_options = {
 			'maxiter': 100,
 			'disp': False,
 		}
 	
-	result = optimize.minimize(softmaxCostCallback, thetaParam, method='L-BFGS-B', jac=True, options=options)
+	result = optimize.minimize(softmaxCostCallback, thetaParam, method='L-BFGS-B', jac=True, options=softmax_options)
 	
 	saeSoftmaxOptTheta = result.x[0:numClasses*hiddenSizeL2]
 	
@@ -103,12 +108,31 @@ stack[1].b = sae2OptTheta[2*hiddenSizeL2*hiddenSizeL1:2*hiddenSizeL2*hiddenSizeL
 (stackParams, netConfig) = stacked_autoencoder.stack2params(stack)
 stackedAETheta = concatenate([saeSoftmaxOptTheta, stackParams])
 
+saeOptThetaFilename = results_dir + 'saeOptTheta.npy'
+
+if os.path.exists(saeOptThetaFilename):
+	stackedAEOptTheta = load(saeOptThetaFilename)
+else:
+	def stackedAutoencoderCostCallback(x):
+		return stacked_autoencoder.cost(x, inputSize, hiddenSizeL2, numClasses, netConfig,
+				lambdaParam, trainData, trainLabels)
+
+	result = optimize.minimize(stackedAutoencoderCostCallback, stackedAETheta, method='L-BFGS-B', jac=True, options=options)
+	
+	stackedAEOptTheta = result.x
+	save(saeOptThetaFilename, stackedAEOptTheta)
+
 # Test
 
 testData = loadMNISTImages('mnist/t10k-images-idx3-ubyte')
 testLabels = loadMNISTLabels('mnist/t10k-labels-idx1-ubyte')
 
-pred = stacked_autoencoder.predict(stackedAETheta, inputSize, hiddenSizeL2, numClasses, netConfig, testData)
+pred_no_fine_tuning = stacked_autoencoder.predict(stackedAETheta, inputSize, hiddenSizeL2, numClasses, netConfig, testData)
 
-acc = mean(testLabels==pred)
-print('Before Finetuning Test Accuracy: %0.3f%%\n' % (acc * 100))
+acc_no_fine_tuning = mean(testLabels==pred_no_fine_tuning)
+print('Before Fine-tuning Test Accuracy: %0.3f%%\n' % (acc_no_fine_tuning * 100))
+
+pred_fine_tuned = stacked_autoencoder.predict(stackedAEOptTheta, inputSize, hiddenSizeL2, numClasses, netConfig, testData)
+
+acc_fine_tuned = mean(testLabels==pred_fine_tuned)
+print('Adter Fine-tuning Test Accuracy: %0.3f%%\n' % (acc_fine_tuned * 100))
